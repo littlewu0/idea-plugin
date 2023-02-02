6e3093a247a997f1862fcd60729d03efb04ad07d
GitHub Action <action@github.com>
2023-02-02 07:35:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/littlewu0/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/littlewu0/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.littlewu0.ideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.littlewu0.ideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
